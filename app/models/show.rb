f2147def3e50caf16ff6c81a732565f4f555896d
Katrina Pritchard <pritckat@gmail.com>
2018-09-05 16:32:26 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,24 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
+    show = self.highest_rating
+    show.name
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    show = self.lowest_rating
+    show.name
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where(["rating > :rating", {rating: 5}])
+  end
 end
